9af2ac878674da5c90a1f6965d774fd13e401441
fantinorec <fantinorecalde@gmail.com>
2026-06-19 20:56:13 -0300

    Update mailer.php

--- backend/config/mailer.php
+++ backend/config/mailer.php
@@ -1,22 +1,21 @@
 <?php
-// backend/api/config/mailer.php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Carga de dependencias de la librería
 require __DIR__ . '/../../vendor/autoload.php';
 
 function enviarCorreo($para, $asunto, $cuerpoHTML) {
     $mail = new PHPMailer(true);
 
     try {
-        // 1. Configuración del Servidor SMTP de Google
+
         $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';                   // Servidor oficial de Gmail
-        $mail->SMTPAuth   = true;                               // Habilitar autenticación SMTP
-        $mail->Username   = 'tu_correo_real@gmail.com';         // 👈 TU DIRECCIÓN DE GMAIL REAL
-        $mail->Password   = 'abcd efgh ijkl mnop';              // 👈 LA CONTRASEÑA DE 16 LETRAS QUE CREASTE
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;     // Cifrado TLS requerido por Google
+        $mail->Host       = 'smtp.gmail.com';                   
+        $mail->SMTPAuth   = true;                               
+        $mail->Username   = 'tu_correo_real@gmail.com';         
+        $mail->Password   = 'abcd efgh ijkl mnop';              
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;     
         $mail->Port       = 587;                                
     
         $mail->setFrom('tu_correo_real@gmail.com', 'Biblioteca Global');
